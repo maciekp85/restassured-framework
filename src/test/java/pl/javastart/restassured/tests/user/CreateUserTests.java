916fbe3eec25d57cc1dbd75c5d04741d69d90f71
Maciej Piotrowski <maciej_piotrowski@epam.com>
2023-10-30 18:51:51 +0100

    FIRST rules - continue, cleaning ap after tests

--- src/test/java/pl/javastart/restassured/tests/user/CreateUserTests.java
+++ src/test/java/pl/javastart/restassured/tests/user/CreateUserTests.java
@@ -1,5 +1,6 @@
 package pl.javastart.restassured.tests.user;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import pl.javastart.restassured.main.pojo.ApiResponse;
 import pl.javastart.restassured.main.pojo.user.User;
@@ -11,10 +12,12 @@ import static org.testng.Assert.assertEquals;
 
 public class CreateUserTests extends SuiteTestBase {
 
+    private User user;
+
     @Test
     public void givenUserWhenPostUserThenUserIsCreatedTest() {
         UserTestDataGenerator userTestDataGenerator = new UserTestDataGenerator(); // Create generator
-        User user = userTestDataGenerator.generateUser(); // Generate User object
+        user = userTestDataGenerator.generateUser(); // Generate User object
 
         ApiResponse apiResponse = given().contentType("application/json")
                 .body(user)
@@ -24,6 +27,15 @@ public class CreateUserTests extends SuiteTestBase {
         assertEquals(apiResponse.getCode(), Integer.valueOf(200), "Code is not as expected");
         assertEquals(apiResponse.getType(), "unknown", "Type is not as expected");
         assertEquals(apiResponse.getMessage(), user.getId().toString(), "Message is not as expected");
+    }
 
+    @AfterMethod
+    public void cleanUpAfterTest() {
+        ApiResponse apiResponse = given().contentType("application/json")
+                .when().delete("user/{username}", user.getUsername())
+                .then().statusCode(200).extract().as(ApiResponse.class);
+        assertEquals(apiResponse.getCode(), Integer.valueOf(200), "Code");
+        assertEquals(apiResponse.getType(), "unknown", "Type");
+        assertEquals(apiResponse.getMessage(), user.getUsername(), "Message");
     }
 }
