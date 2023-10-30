02892a18bd250a6f96ba792a0769949919232711
Maciej Piotrowski <maciej_piotrowski@epam.com>
2023-10-30 18:41:08 +0100

    Exercise F.2

--- src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
+++ src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import pl.javastart.restassured.main.pojo.pet.Category;
 import pl.javastart.restassured.main.pojo.pet.Pet;
 import pl.javastart.restassured.main.pojo.pet.Tag;
+import pl.javastart.restassured.main.test.data.PetTestDataGenerator;
 import pl.javastart.restassured.tests.testbases.SuiteTestBase;
 
 import java.util.Collections;
@@ -16,20 +17,7 @@ public class CreatePetTests extends SuiteTestBase {
     @Test
     public void givenPetWhenPostPetThenPetIsCreatedTest() {
 
-        Category category = new Category();
-        category.setId(1);
-        category.setName("dogs");
-
-        Tag tag = new Tag();
-        tag.setId(1);
-        tag.setName("dogs-category");
-
-        Pet pet = new Pet();
-        pet.setId(123);
-        pet.setCategory(category);
-        pet.setPhotoUrls(Collections.singletonList("http://photos.com/dog1.jpg"));
-        pet.setTags(Collections.singletonList(tag));
-        pet.setStatus("available");
+        Pet pet = new PetTestDataGenerator().generatePet();
 
         Pet actualPet = given().body(pet).contentType("application/json")
                 .when().post("pet")
