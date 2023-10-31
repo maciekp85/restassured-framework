aa3f87277f31906b8d35ed80544ce6c419a1924a
Maciej Piotrowski <maciej_piotrowski@epam.com>
2023-10-31 10:57:31 +0100

    Exercise F.3

--- src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
+++ src/test/java/pl/javastart/restassured/tests/pet/CreatePetTests.java
@@ -1,29 +1,39 @@
 package pl.javastart.restassured.tests.pet;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import pl.javastart.restassured.main.pojo.pet.Category;
+import pl.javastart.restassured.main.pojo.ApiResponse;
 import pl.javastart.restassured.main.pojo.pet.Pet;
-import pl.javastart.restassured.main.pojo.pet.Tag;
 import pl.javastart.restassured.main.test.data.PetTestDataGenerator;
 import pl.javastart.restassured.tests.testbases.SuiteTestBase;
-
-import java.util.Collections;
 
 import static io.restassured.RestAssured.given;
 import static org.testng.Assert.assertEquals;
 
 public class CreatePetTests extends SuiteTestBase {
 
+    private Pet actualPet;
+
     @Test
     public void givenPetWhenPostPetThenPetIsCreatedTest() {
 
         Pet pet = new PetTestDataGenerator().generatePet();
 
-        Pet actualPet = given().body(pet).contentType("application/json")
+        actualPet = given().body(pet).contentType("application/json")
                 .when().post("pet")
                 .then().statusCode(200).extract().as(Pet.class);
 
         assertEquals(actualPet.getId(), pet.getId(), "Pet id");
         assertEquals(actualPet.getName(), pet.getName(), "Pet name");
+    }
+
+    @AfterMethod
+    public void cleanUpAfterTest() {
+        ApiResponse apiResponse = given().contentType("application/json")
+                .when().delete("pet/{petId}", actualPet.getId())
+                .then().statusCode(200).extract().as(ApiResponse.class);
+        assertEquals(apiResponse.getCode(), Integer.valueOf(200), "Code");
+        assertEquals(apiResponse.getType(), "unknown", "Type");
+        assertEquals(apiResponse.getMessage(), actualPet.getId().toString(), "Message");
     }
 }
